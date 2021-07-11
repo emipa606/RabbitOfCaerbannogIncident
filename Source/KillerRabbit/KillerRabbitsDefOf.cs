18af10674cb60c02672583941a4a46c452484ebf
emipa606 <emil.offermo@litet.net>
2021-07-11 20:04:57 +0200

    Mod updated for 1.3 and passed autotests

--- Source/KillerRabbit/KillerRabbitsDefOf.cs
+++ Source/KillerRabbit/KillerRabbitsDefOf.cs
@@ -5,9 +5,9 @@ namespace KillerRabbits
 {
     // Token: 0x02000004 RID: 4
     [DefOf]
-	public static class KillerRabbitsDefOf
-	{
-		// Token: 0x04000001 RID: 1
-		public static PawnKindDef Rabbit_of_Caerbannog;
-	}
+    public static class KillerRabbitsDefOf
+    {
+        // Token: 0x04000001 RID: 1
+        public static PawnKindDef Rabbit_of_Caerbannog;
+    }
 }
